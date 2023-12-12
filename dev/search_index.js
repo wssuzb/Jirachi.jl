449035455866b7c4428b22456b0607b39e4782ea
Documenter.jl <documenter@juliadocs.github.io>
2023-12-12 03:12:45 +0000

    build based on be53f0f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Jirachi","category":"page"},{"location":"#Jirachi","page":"Home","title":"Jirachi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Jirachi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Jirachi]","category":"page"}]
+}
