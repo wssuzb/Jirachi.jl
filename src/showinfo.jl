a6cfe2a2b84890cd4a9ca01005ee88245e964901
wssuzb <wssuzb@gmail.com>
2023-12-26 20:43:54 +0800

    add fractional var info

--- src/showinfo.jl
+++ src/showinfo.jl
@@ -5,6 +5,7 @@ nanmedian(arr) = median(filter(!isnan, arr))
 function Base.show(io::IO, lc::lightcurve) # ::MIME"text/plain", 
     
     flux_err_mag = flux2mag(lc.flux, lc.err)
+    frac_var = fractional_variability(lc.flux, lc.err)
 
     println(io, "Loading $lightcurve, band used: ", lc.band, "-band")
     println(" ")
@@ -24,4 +25,8 @@ function Base.show(io::IO, lc::lightcurve) # ::MIME"text/plain",
     println(io, "\t\t median error: ", nanmedian(lc.err))
     println(io, "\t\t mean mag err / 1e-2: ", round.(nanmean(flux_err_mag[2] / 1e-2), digits=2))
     println(io, "\t\t std mag err / 1e-2: ", round.(std(flux_err_mag[2] / 1e-2), digits=2))
+    println(" ")
+    println(io, "\t Fractional Variability: ")
+    println(io, "\t\t f_var: ", round(frac_var[1], digits=2))
+    println(io, "\t\t f_var error: ", round(frac_var[2], digits=2))
 end
