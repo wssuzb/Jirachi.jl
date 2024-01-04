55f77a6ae102274fbd2f0fc05956930ee614cb98
wssuzb <wssuzb@gmail.com>
2024-01-04 11:48:42 +0800

    add py version

--- py/run_sample.py
+++ py/run_sample.py
@@ -1,17 +1,16 @@
+#%%
+import variation as var
 import sys
 import warnings
-from scipy.signal import argrelextrema
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import binned_statistic
 from matplotlib.ticker import ScalarFormatter
 import matplotlib.ticker as ticker
 from scipy.optimize import curve_fit
-from scipy import interpolate
 from scipy import stats
-
-import h5py
+import matplotlib
+# print(matplotlib.rcParams['mathtext.fontset'])
 warnings.filterwarnings("ignore")
 # matplotlib.use('agg')
 plt.rcParams.update({
@@ -38,184 +37,164 @@ plt.rcParams.update({
     'ytick.minor.width': 0.6,
     "text.usetex": True,
     "axes.titlesize":"medium",
-    "figure.dpi" : 500
+    # "figure.dpi" : 500
 })
-def geterror(x,perclim=84.1):
-    """
-    x: tau list.
-    return:
-        tau, tau_lerr, tau_uperr
-    """
-    tau = stats.scoreatpercentile(x, 50)
-    centau_uperr = (stats.scoreatpercentile(x, perclim))-tau
-    centau_loerr = tau-(stats.scoreatpercentile(x, (100.-perclim)))
-    return [round(tau,3), round(centau_loerr,3), round(centau_uperr,3)]
-    
-# f = h5py.File('./run_result/run_bin_103.68_montano22_n1_nsim_1000_mode_both_nsigma_3_band_i_z_sun14.h5', 'r') 
-f = h5py.File('./run_i_z.h5', 'r')
 
-fit = f['fit']
-band1, band2 = f['band'].value.flatten()
+band = ["g", "r", "i", "z"]
 
-num_all = f['num_all'].value
-num_cut = f['num_cut'].value
-num_pos = f['num_pos'].value
+band_pair = []
 
-err1, err2 = f['par'].value[0][6], f['par'].value[1][6]
+for i in range(len(band)):
+   for j in range(1, len(band)):
+       if i<j:
+           band_pair.append([band[i], band[j]])
+       
 
-t1_min, t2_min = f['t_min']
-t1_min, t2_min = 10 ** t1_min, 10 ** t2_min
+used_band = ['g', 'r']
 
-sf1_min, sf2_min = f['sf_min']
+t_cad = 103.68 #mean(diff(lc.time))
+t_start = 0 - (t_cad / 2) #lc.time[1] - (t_cad / 2)
+t_end = 25000 + (t_cad /2 )
+lc_edges = np.arange(t_start, t_end + t_cad, t_cad)
+used_bins = lc_edges
 
-t1_max, t2_max = f['t_max']
-t1_max, t2_max = 10 ** t1_max, 10 ** t2_max
+sf_bin = 10 ** np.arange(0, 5.05, 0.05)
+cv_bin = 10 ** np.arange(0, 5.1, 0.1)
+
+lc1_dat = np.loadtxt("../../../archive_data/ngc4395/Montano22_night1/%s_4395.txt"%str(used_band[0]), usecols=[0, 1, 2])
+lc2_dat = np.loadtxt("../../../archive_data/ngc4395/Montano22_night1/%s_4395.txt"%str(used_band[1]), usecols=[0, 1, 2])
+
+t1, y1, e1 = lc1_dat[:, 0], lc1_dat[:, 1], lc1_dat[:, 2]
+t1 = (t1 - t1[0]) * 24 * 3600 - 1e-9
+# t1, y1, e1 = np.array(t1), np.array(y1), np.array(e1)
+lc1 = var.lightcurve(t1, y1, e1)
+
+t2, y2, e2 = lc2_dat[:, 0], lc2_dat[:, 1], lc2_dat[:, 2]
+t2 = (t2 - t2[0]) * 24 * 3600 - 1e-9
+# t2, y2, e2 = np.array(t2), np.array(y2), np.array(e2)
+lc2 = var.lightcurve(t2, y2, e2)
+
+lc1_bin = var.bin_lightcurve(lc1, lc_edges)
+lc2_bin = var.bin_lightcurve(lc2, lc_edges)
+
+binsf1 = var.structure_function_mcmc(lc1_bin, sf_bin, mode="both", nsim=1000)
+binsf2 = var.structure_function_mcmc(lc2_bin, sf_bin, mode="both", nsim=1000)
+
+
+fit1_res = var.find_proper_time(binsf1, p0 = [1, 1e3, 1, 0.005],  lower_bounds = [0, 0, 0, 0.001], upper_bounds = [10, 2e4, 2, 0.01 ],)
+
+t1_max, sf1_max, t1_min, sf1_min, t1_fit, sf1_fit, par1, par1_err = fit1_res.t_max, fit1_res.sf_max, fit1_res.t_min, fit1_res.sf_min, fit1_res.t_fit, fit1_res.sf_fit, fit1_res.par, fit1_res.par_err
+
+
+fit2_res = var.find_proper_time(binsf2, p0 = [1, 1e3, 1, 0.005],lower_bounds = [0, 0, 0, 0.001], upper_bounds = [10, 2e4, 2, 0.01 ], )
+
+t2_max, sf2_max, t2_min, sf2_min, t2_fit, sf2_fit, par2, par2_err = fit2_res.t_max, fit2_res.sf_max, fit2_res.t_min, fit2_res.sf_min, fit2_res.t_fit, fit2_res.sf_fit, fit2_res.par, fit2_res.par_err
+
+"""
+get common light curves 
+"""
+
+lc1_new, lc2_new = var.get_common_lc(lc1_bin, lc2_bin)
+
+cv_flux = var.color_variation(lc1_new, lc2_new, erron=True, nsigma=3, used_bins=10 ** np.arange(0, 5.1, 0.1), showhist=True)
+
+cv_mag = var.color_variation(lc1_new, lc2_new, erron=True, nsigma=3, used_bins=10 ** np.arange(0, 5.1, 0.1), mode='mag')
+
+band1, band2 = used_band[0], used_band[1]
+
 
 t_used_min = max(t1_min, t2_min)
 t_used_max = min(t1_max, t2_max)
 
-flux_ratio = f['flux_ratio'].value.flatten()
+flux_ratio = [0.9] #f['flux_ratio'].value.flatten()
 
-
-sf = f['sf'].value
-bin_sf_1_t = 10 ** sf[0][0]
-bin_sf_1_t_err = 10 ** sf[0][1]
-bin_sf_1_sf = sf[0][2]
-bin_sf_1_sf_err = sf[0][3]
-# bin_sf_1_sf_err_hig = sf[0][4]
+bin_sf_1_t = binsf1.x
+bin_sf_1_t_err = binsf1.xerr
+bin_sf_1_sf = binsf1.y
+bin_sf_1_sf_err = binsf1.yerr
 
 idx_br_1 = np.where(bin_sf_1_t <= t1_max, True, False)
 
-bin_sf_2_t = 10 ** sf[1][0]
-bin_sf_2_t_err = 10 ** sf[1][1]
-bin_sf_2_sf = sf[1][2]
-bin_sf_2_sf_err = sf[1][3]
-# bin_sf_2_sf_err_hig = sf[1][4]
+bin_sf_2_t = binsf2.x
+bin_sf_2_t_err = binsf2.xerr
+bin_sf_2_sf = binsf2.y
+bin_sf_2_sf_err = binsf2.yerr
 
 idx_br_2 = np.where(bin_sf_2_t <= t2_max, True, False)
 
-
 # flux-flux
 
-cv_1_t = 10 ** f['cv'][0][0]
+num_all = cv_flux.num_all
+num_cut = cv_flux.num_cut
+num_pos = cv_flux.num_pos
 
-cv_1_t_err = 10 ** f['cv'][0][1]
+cv_1_t = cv_flux.x
+
+cv_1_t_err = cv_flux.xerr
 # cv_1_t_err = [(cv_1_t[i+1] - cv_1_t[i]) / 2 for i in range(len(cv_1_t)- 1)]
-cv_1_y = f['cv'][0][2]
-cv_1_y_err = f['cv'][0][3]
+cv_1_y = cv_flux.y
+cv_1_y_err = cv_flux.yerr
 
 idx_cv_1 = np.where((cv_1_t>=t_used_min) & (cv_1_t<=t_used_max), True, False)
 
-
 # mag-mag
 
-cv_2_t = 10 ** f['cv'][1][0]
-cv_2_t_err = 10 ** f['cv'][1][1]
-cv_2_y = f['cv'][1][2]
-cv_2_y_err = f['cv'][1][3]
+cv_2_t = cv_mag.x
+cv_2_t_err = cv_mag.xerr
+cv_2_y = cv_mag.y
+cv_2_y_err = cv_mag.yerr
 
 idx_cv_2 = np.where((cv_2_t>=t_used_min) & (cv_2_t<=t_used_max), True, False)
 
-
-
 cv_use_bin= 10 ** np.arange(0., 5.1, 0.1)
-# cv_use_bin= 10 ** np.arange(0., 5.2, 0.2)
 bin_width_all = [(cv_use_bin[i+1] - cv_use_bin[i]) / 2 for i in range(len(cv_use_bin)- 1)]
 xerr_all = np.vstack((bin_width_all, bin_width_all))
 bin_width_all = np.array(bin_width_all)
 bin_centers_all = cv_use_bin[1:] - bin_width_all
 
 
-"""
-TODO ERROR.
-"""
-
 # For plotting -------------------------------------------------------------->
-mosaic = ['A', 'B']
 figure_mosaic = """
 AC
 BD
 """
-colors = ['xkcd:mid blue', 'darkred']
+
+colors = [(46/255, 89/255, 167/255),'xkcd:orange red']
 
 fig, axes = plt.subplot_mosaic(mosaic = figure_mosaic, figsize=(4.2, 3.5))
 
-
-axes['A'].axhline(np.sqrt(2) * err1, lw=0.2, ls='-', color=colors[0])
-axes['A'].axhline(np.sqrt(2) * err2, lw=0.2, ls='--', color=colors[1])
-
+axes['A'].axhline(np.sqrt(2) * par1[-1], lw=0.2, ls='-', color=colors[0])
+axes['A'].axhline(np.sqrt(2) * par2[-1], lw=0.2, ls=(0, (15, 3)), color=colors[1])
 axes['A'].scatter(t1_min, sf1_min, marker='x', zorder=3, s=8, linewidth=1.2, color='blue')
 axes['A'].scatter(t2_min, sf2_min, marker='x', zorder=3, s=8, linewidth=1.2, color='red')
-
 axes['A'].scatter(bin_sf_1_t[idx_br_1], bin_sf_1_sf[idx_br_1], 
                   marker='^', s=5, color='black', linewidth=0.2,facecolors='none',)
 axes['A'].scatter(bin_sf_1_t[~idx_br_1], bin_sf_1_sf[~idx_br_1], 
                   marker='^', s=5, linewidth=0.2, facecolors='none', color='gray')
-
 axes['A'].errorbar(bin_sf_1_t[idx_br_1], bin_sf_1_sf[idx_br_1],
                    yerr=np.vstack((bin_sf_1_sf_err[idx_br_1], bin_sf_1_sf_err[idx_br_1])),
                    ms=1, ls='none',mew=0.2, elinewidth=0.1, mec='black',ecolor='black', capsize=0.,mfc='none')
-
 axes['A'].errorbar(bin_sf_1_t[~idx_br_1], bin_sf_1_sf[~idx_br_1],
                    yerr=np.vstack((bin_sf_1_sf_err[~idx_br_1], bin_sf_1_sf_err[~idx_br_1])),
                    ms=2, ls='none', mew=0.2, elinewidth=0.1, mec='gray', ecolor='gray',mfc='none', capsize=0.)
-
 axes['A'].scatter(bin_sf_2_t[idx_br_2], bin_sf_2_sf[idx_br_2], 
                   marker='s', s=5, color='black', linewidth=0.2,facecolors='none',)
 axes['A'].scatter(bin_sf_2_t[~idx_br_2], bin_sf_2_sf[~idx_br_2], 
                   marker='s', s=5, linewidth=0.2, facecolors='none', color='gray')
-
 axes['A'].errorbar(bin_sf_2_t[idx_br_2], bin_sf_2_sf[idx_br_2],
                    yerr=np.vstack((bin_sf_2_sf_err[idx_br_2], bin_sf_2_sf_err[idx_br_2])),
                    ms=1, ls='none',mew=0.2, elinewidth=0.1, mec='black',ecolor='black', capsize=0.,mfc='none')
 axes['A'].errorbar(bin_sf_2_t[~idx_br_2], bin_sf_2_sf[~idx_br_2],
                    yerr=np.vstack((bin_sf_2_sf_err[~idx_br_2], bin_sf_2_sf_err[~idx_br_2])),
                    ms=2, ls='none', mew=0.2, elinewidth=0.1, mec='gray', ecolor='gray',mfc='none', capsize=0.)
-
-axes['A'].plot(fit[0][0], fit[0][1], lw=0.6, color=colors[0])#, label=r'$%s$-band'%str(band_pair[0]))
-axes['A'].plot(fit[1][0], fit[1][1], lw=0.6, color=colors[1], ls='--')#, label=r'$%s$-band'%str(band_pair[0]))
-
-
+axes['A'].plot(t1_fit, sf1_fit, lw=0.6, color=colors[0])
+axes['A'].plot(t2_fit, sf2_fit, lw=0.6, color=colors[1], ls=(0, (5, 3)))#, 
 axes['A'].set_yscale('log')
 axes['A'].set_ylabel('SF [mag]', fontsize=10)
-
 axes['A'].set_ylim(6e-3, 1e-1)
-
 axes['A'].yaxis.set_major_formatter(ScalarFormatter()) 
 axes['A'].set_yticklabels(['','', 0.01, 0.1])
-
-
-axes['B'].errorbar(cv_1_t[~idx_cv_1], cv_1_y[~idx_cv_1],
-                   xerr= bin_width_all[~idx_cv_1], 
-                   yerr=cv_1_y_err[~idx_cv_1], fmt='o', mfc='none',mew=0.2,elinewidth=0.2, ms=2, lw=0.5,ls='none',
-                   color='black', zorder=2, alpha=0.5,capsize=0.5)
-
-axes['B'].scatter(cv_1_t[idx_cv_1], cv_1_y[idx_cv_1], 
-                  marker='o', s=5,  linewidth=0.2, color='magenta')#facecolors='none', 
-
-axes['B'].errorbar(cv_1_t[idx_cv_1], cv_1_y[idx_cv_1],
-                   xerr= np.vstack((bin_width_all[idx_cv_1], bin_width_all[idx_cv_1])), 
-                   yerr=np.vstack((cv_1_y_err[idx_cv_1], cv_1_y_err[idx_cv_1])), color='black',
-                   mew=0.2, elinewidth=0.2, lw=0.3,ls='-', capsize=0.5)#, label='%s vs. %s'%(band_pair[0], band_pair[1]), color='black', ecolor='black', zorder=2)
-
-axes['B'].axhline(y=1 / flux_ratio[0], xmin=0, xmax=1e4, color='gray', ls='--', zorder=0, lw=0.5) # ewidth
-
-
-axes['B'].set_ylabel(r'$C_f(\tau)$', fontsize=10)
-
-axes['B'].set_yticks(np.arange(0.30, 1.5, 0.1), major=True)
-axes['B'].set_yticks(np.arange(0.30, 1.52, 0.02), minor=True)
-axes['B'].set_ylim(0.5, 1.1)
-
-
-axes['B'].text(0.05, 0.05, 'BWB', transform=axes['B'].transAxes, color='blue', fontsize=7)
-axes['B'].text(0.05, 0.9, 'RWB', transform=axes['B'].transAxes, color='red', fontsize=7)
-
-
-axes['B'].set_xlabel(r'$\tau$ [sec]', fontsize=10)
 axes['A'].text(0.9, 0.9, '(a)', transform=axes['A'].transAxes, color='black', fontsize=7)
-axes['B'].text(0.9, 0.9, '(c)', transform=axes['B'].transAxes, color='black', fontsize=7)
-
 axes['A'].text(0.8, 0.1, r'$\sqrt{2}\sigma_n^i$', transform=axes['A'].transAxes, color=colors[0], fontsize=5)
 axes['A'].text(0.8, 0.2, r'$\sqrt{2}\sigma_n^z$', transform=axes['A'].transAxes, color=colors[1], fontsize=5)
 
@@ -229,15 +208,34 @@ axes['A'].scatter(150, 0.063, marker='^', s=8, color='black',
 axes['A'].plot([130, 170], [0.063, 0.063], ls='-', color=colors[0], lw=0.5)
 axes['A'].text(200, 0.06, r'$%s$-band'%str(band1), color=colors[0], fontsize=6)
 axes['A'].text(195, 0.045, r'$%s$-band'%str(band2),color=colors[1], fontsize=6)
-
 plt.setp(axes['A'].get_xticklabels(), visible=False)
+
+axes['B'].errorbar(cv_1_t[~idx_cv_1], cv_1_y[~idx_cv_1],
+                   xerr= bin_width_all[~idx_cv_1], 
+                   yerr=cv_1_y_err[~idx_cv_1], fmt='o', mfc='none',mew=0.2,elinewidth=0.2, ms=2, lw=0.5,ls='none',
+                   color='black', zorder=2, alpha=0.5,capsize=0.5)
+axes['B'].scatter(cv_1_t[idx_cv_1], cv_1_y[idx_cv_1], 
+                  marker='o', s=5,  linewidth=0.2, color='magenta')#facecolors='none', 
+axes['B'].errorbar(cv_1_t[idx_cv_1], cv_1_y[idx_cv_1],
+                   xerr= np.vstack((bin_width_all[idx_cv_1], bin_width_all[idx_cv_1])), 
+                   yerr=np.vstack((cv_1_y_err[idx_cv_1], cv_1_y_err[idx_cv_1])), color='black',
+                   mew=0.2, elinewidth=0.2, lw=0.3,ls='-', capsize=0.5)#, label='%s vs. %s'%(band_pair[0], band_pair[1]), color='black', ecolor='black', zorder=2)
+axes['B'].axhline(y=1 / flux_ratio[0], xmin=0, xmax=1e4, color='gray', ls='--', zorder=0, lw=0.5) # ewidth
+axes['B'].set_ylabel(r'$C_f(\tau)$', fontsize=10)
+axes['B'].set_yticks(np.arange(0.30, 1.5, 0.1), major=True)
+axes['B'].set_yticks(np.arange(0.30, 1.52, 0.02), minor=True)
+axes['B'].set_ylim(0.5, 1.1)
+axes['B'].text(0.05, 0.05, 'BWB', transform=axes['B'].transAxes, color='blue', fontsize=7)
+axes['B'].text(0.05, 0.9, 'RWB', transform=axes['B'].transAxes, color='red', fontsize=7)
+axes['B'].set_xlabel(r'$\tau$ [sec]', fontsize=10)
+axes['B'].text(0.9, 0.9, '(c)', transform=axes['B'].transAxes, color='black', fontsize=7)
 
 
 use_bin = 10 ** np.arange(1, 5.2, 0.1)
 axes['C'].hist(num_all, bins=use_bin, histtype='step', color='black', ls=(5, (11, 3)),lw=0.5, label='All pairs')
 axes['C'].hist(num_cut, bins=use_bin, histtype='step',ls=(0, (1, 0.5)), lw=0.5, color='blue',  label=r'$3\sigma$-cut', zorder=4)
 axes['C'].hist(num_pos, bins=use_bin, histtype='step',lw=0.7, color='magenta', label=r'$3\sigma$-${\rm cut}~\&~C_{f/m}>0$', zorder=3)
-# axes['A'].legend(loc='upper left', frameon=False, fontsize=5)
+axes['C'].legend(loc='upper left', frameon=False, fontsize=5)
 axes['C'].set_ylabel('number of pairs', fontsize=10)
 axes['C'].set_yscale('log')
 axes['C'].set_ylim(2, 3e4)
@@ -261,8 +259,7 @@ axes['D'].scatter(cv_2_t[idx_cv_2], cv_2_y[idx_cv_2],
 axes['D'].errorbar(cv_2_t[idx_cv_2], cv_2_y[idx_cv_2],
                    xerr= np.vstack((bin_width_all[idx_cv_2], bin_width_all[idx_cv_2])), 
                    yerr=np.vstack((cv_2_y_err[idx_cv_2], cv_2_y_err[idx_cv_2])), color='black',
-                   mew=0.2, elinewidth=0.2, lw=0.3,ls='-', capsize=0.5)#, label='%s vs. %s'%(band_pair[0], band_pair[1]), color='black', ecolor='black', zorder=2)
-
+                   mew=0.2, elinewidth=0.2, lw=0.3,ls='-', capsize=0.5)
 
 axes['D'].text(0.05, 0.05, 'BWB', transform=axes['D'].transAxes, color='blue', fontsize=7)
 axes['D'].text(0.05, 0.9, 'RWB', transform=axes['D'].transAxes, color='red', fontsize=7)
@@ -270,36 +267,28 @@ axes['D'].text(0.05, 0.9, 'RWB', transform=axes['D'].transAxes, color='red', fon
 axes['D'].set_yticks(np.arange(0.5, 1.5, 0.1), major=True)
 axes['D'].set_yticks(np.arange(0.5, 1.5, 0.02), minor=True)
 axes['D'].set_ylim(0.7,1.3)
-# axes['B'].set_ylabel(r'$\bar{\vartheta}(\tau)$ [deg]', fontsize=10)
 axes['D'].set_ylabel(r'$C_m(\tau)$', fontsize=10)
 
 plt.setp(axes['A'].get_xticklabels(), visible=False)
 plt.setp(axes['C'].get_xticklabels(), visible=False)
-# plt.setp(axes['B'].get_xticklabels(), visible=False) 
+
 for ax in ['A', 'B', 'C', 'D']:#, 'C'
     axes[ax].set_xscale('log')
     axes[ax].set_xlim(80, 3e4)
     axes[ax].tick_params(axis='x', labelsize=8)
     axes[ax].tick_params(axis='y', labelsize=8)
-    
+
+color_filled = "palevioletred"
+
 axes['A'].fill_between(np.arange(t_used_min, t_used_max, 0.1), 2e-3, 1e-1,
-                       facecolor='palevioletred', alpha=0.15,zorder=0)
+                       facecolor=color_filled, alpha=0.15, zorder=0)
 axes['B'].fill_between(np.arange(t_used_min, t_used_max, 0.1), 0., 2,
-                       facecolor='palevioletred', alpha=0.15, zorder=0)
+                       facecolor=color_filled, alpha=0.15, zorder=0)
 axes['C'].fill_between(np.arange(t_used_min, t_used_max, 0.1), 1, 3e4,
-                       facecolor='palevioletred', alpha=0.15, zorder=0)
+                       facecolor=color_filled, alpha=0.15, zorder=0)
 axes['D'].fill_between(np.arange(t_used_min, t_used_max, 0.1), 0, 2,
-                       facecolor='palevioletred', alpha=0.15, zorder=0)
-                       
-# ==========================================================================
-
-
-axes['C'].legend(loc='upper left', frameon=False, fontsize=4)
-
-
-# ==========================================================================
+                       facecolor=color_filled, alpha=0.15, zorder=0)
+           
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.3, hspace=0.03)
-# plt.savefig('./figure/mag_flux_new_sun14_test.png', dpi=500, bbox_inches='tight')
-# plt.savefig('./figure/mag_flux_new_sun14_sf_0.1_cv_0.5.png', dpi=500, bbox_inches='tight')
-plt.show()
+plt.savefig('./mag_flux_new_sun14_%s_%s.png'%(str(used_band[0]), str(used_band[1])), dpi=500, bbox_inches='tight')
---
+++ py/variation.py
@@ -0,0 +1,267 @@
+#!/bin/python
+
+import numpy as np
+from scipy.stats import binned_statistic
+from scipy.signal import find_peaks
+from scipy.optimize import curve_fit
+from collections import namedtuple
+import matplotlib.pyplot as plt
+
+
+lightcurve = namedtuple('lc', ('time', 'flux', 'err'))
+binned_data = namedtuple('bin', ('x', 'xerr', 'y', 'yerr'))
+binned_data_cv = namedtuple('bin', ('x', 'xerr', 'y', 'yerr', 'num_all', 'num_cut', 'num_pos'))
+fit_result = namedtuple('fit', ('t_max', 'sf_max', 't_min', 'sf_min', 't_fit', 'sf_fit', 'par', 'par_err'))
+
+def bin_lightcurve(lc, used_bins):
+    
+    if not isinstance(lc, lightcurve):
+        print("light curves should be input with namedtuple-like type!!!")
+        
+    bin_dt_width = np.array([(used_bins[i+1] - used_bins[i]) / 2 for i in range(len(used_bins) - 1)])
+    bin_dt_center = used_bins[1:] - bin_dt_width
+
+    bin_flux = binned_statistic(lc.time, lc.flux, statistic='mean', bins=used_bins)[0]
+
+    bin_err = binned_statistic(lc.time, lc.err, statistic=lambda x: np.sqrt(np.sum(np.array(x) ** 2)) / len(x), bins=used_bins)[0]
+
+    idx = np.isnan(bin_flux)
+
+    return lightcurve(bin_dt_center[~idx], bin_flux[~idx], bin_err[~idx])
+
+
+def get_common_lc(lc1, lc2):
+    
+    common_t = np.intersect1d(lc1.time, lc2.time, return_indices=True)
+    
+    return lightcurve(lc1.time[common_t[1]], lc1.flux[common_t[1]], lc1.err[common_t[1]]), lightcurve(lc2.time[common_t[2]], lc2.flux[common_t[2]], lc2.err[common_t[2]])
+
+def lc_mcmc(lc, mode='both'):
+        
+    if not isinstance(lc, lightcurve):
+        print("light curves should be input with namedtuple-like type!!!")
+
+
+    numt = len(lc.time)
+    indx = np.random.randint(0, numt, numt)
+    unique, counts = np.unique(indx, return_counts=True) # sorted unique value
+    t_rss = lc.time[unique]
+    y_rss = lc.flux[unique]
+    e_rss = lc.err[unique]/np.sqrt(counts)
+    
+    y_fr_rss = np.random.normal(y_rss, e_rss)
+    idx_sort = np.argsort(t_rss, kind='mergesort')
+    
+    if mode == "rss":
+        return lightcurve(t_rss[idx_sort], y_rss[idx_sort], e_rss[idx_sort])
+    elif mode == "both":
+        return lightcurve(t_rss[idx_sort], y_fr_rss[idx_sort], e_rss[idx_sort])
+    else:
+        return lightcurve(lc.time, np.random.normal(lc.flux, lc.err), lc.err)
+
+def structure_function(lc, used_bins):
+    
+    if not isinstance(lc, lightcurve):
+        print("light curves should be input with namedtuple-like type!!!")
+    
+    time = lc.time    
+    flux = -2.5 * np.log10(lc.flux)
+    
+    time_2d = np.tile(time, (len(time), 1))
+    dt_all = time_2d - np.transpose(time_2d)    
+    x_flux_2d = np.tile(flux, (len(flux), 1))
+    dx_flux_all = x_flux_2d - np.transpose(x_flux_2d)
+    
+    idx = np.where(dt_all>0, True, False)
+
+    dt_all = dt_all[idx]
+    dx_flux_all = dx_flux_all[idx]
+
+    int0 = np.argsort(dt_all, kind='mergesort')
+    dt_all_sort = dt_all[int0]
+    flux_sort = np.sqrt(np.pi / 2 * dx_flux_all[int0] ** 2)
+    
+    bin_dt_width = np.array([(used_bins[i+1] - used_bins[i]) / 2 for i in range(len(used_bins) - 1)])
+    bin_dt_center = used_bins[1:] - bin_dt_width
+
+    bin_sf = binned_statistic(dt_all_sort, flux_sort, statistic=lambda x: np.nanmean(x), bins=used_bins)[0]
+
+    bin_sf_err = binned_statistic(dt_all_sort, flux_sort, statistic=lambda x : np.nanstd(x) / np.sqrt(np.count_nonzero(~np.isnan(x))), bins=used_bins)[0]
+
+    return binned_data(bin_dt_center, bin_dt_width, bin_sf, bin_sf_err)
+
+def structure_function_mcmc(lc,  used_bins, mode='both', nsim=200):
+    
+    if not isinstance(lc, lightcurve):
+        print("light curves should be input with namedtuple-like type!!!")
+    
+    _tmp_sf = np.zeros((nsim, len(used_bins) - 1))
+
+    for i in range(nsim):
+        tmp_lc = lc_mcmc(lc, mode=mode)
+        tmp_sf = structure_function(tmp_lc, used_bins)
+        _tmp_sf[i] = tmp_sf.y
+
+    sf_err_mcmc = np.zeros(len(used_bins) - 1)
+
+    for i in range(len(used_bins) - 1):
+        sf_err_mcmc[i] = np.nanstd(_tmp_sf[:, i])
+
+    mysf = structure_function(lc, used_bins)
+    
+    idx = np.isnan(mysf.y)
+    
+    return binned_data(mysf.x[~idx], mysf.xerr[~idx], mysf.y[~idx], sf_err_mcmc.flatten()[~idx])
+
+
+def fit_sf(t, sf_inf, tau, beta, sigma):
+    return np.sqrt(sf_inf ** 2 * (1-np.exp(-(t / tau) ** beta)) + 2 * sigma ** 2)
+
+def find_t_max(binsf, prominence=1e-3, width=1e-3, check_plot=False):
+    peaks, properties = find_peaks(binsf.y, prominence=prominence, width=width)
+
+    if check_plot:
+        fig = plt.figure(figsize=(6, 4))
+        plt.errorbar(binsf.x, binsf.y, binsf.yerr, label='SF')
+        plt.scatter(binsf.x[peaks], binsf.y[peaks], marker='*', color='red', label='local maximum')
+        plt.xlabel(r'$\Delta t$')
+        plt.ylabel('SF [mag]')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.legend()
+        plt.show()
+
+    return binsf.x[peaks], binsf.y[peaks]
+
+
+def find_proper_time(binsf, prominence=1e-3, width=1e-3,  sf_noise_sigma = 2, p0 = [1, 1e3, 1, 0.005],lower_bounds = [], upper_bounds = [],  t_fit = 10 ** np.arange(0, 5.1, 0.1)):
+    
+    peaks, properties = find_peaks(binsf.y, prominence=prominence, width=width)
+
+    t_max, sf_max = binsf.x[peaks][0], binsf.y[peaks][0]
+    
+    fit_idx = np.where(binsf.x <= t_max, True, False)
+
+    if upper_bounds[1] >= t_max:
+        upper_bounds[1] = t_max
+
+    popt, pcov = curve_fit(fit_sf, binsf.x[fit_idx], binsf.y[fit_idx], sigma= binsf.yerr[fit_idx], p0=p0, bounds=(lower_bounds, upper_bounds))
+    
+    par, par_err = popt, np.sqrt(np.diag(pcov))
+    
+    sf_fit = fit_sf(t_fit, *popt)
+
+    noise_cut = sf_noise_sigma * np.sqrt(2) * popt[-1] 
+
+    t_min = np.interp(noise_cut, sf_fit, t_fit)
+
+    sf_min = noise_cut
+
+    return fit_result(t_max, sf_max, t_min, sf_min, t_fit, sf_fit, par, par_err)
+
+def err_std(x, num=500):
+    res = []
+    x = np.array(x)
+    for i in range(num):
+        idx = np.random.randint(0, len(x), len(x))
+        unique, counts = np.unique(idx, return_counts=True)
+        res.append(np.median(x[unique]))
+    
+    return np.std(res)
+
+def flux2mag(flux, err):
+    mag = -2.5 * np.log10(flux) - 21.175
+    mag_err = 2.5 / np.log(10) * (err / flux) # approximately!!!
+    return mag, mag_err
+
+def color_variation(lc1, lc2, erron, nsigma, used_bins, mode='flux', showhist=False):
+    
+    if not (isinstance(lc1, lightcurve) & isinstance(lc2, lightcurve)):
+        print("light curves should be input with namedtuple-like type!!!")
+    
+    if not (lc1.time == lc2.time).all():
+        print("The Light curves should be quasi-simultaneou observed!!!")
+
+    t1 = lc1.time
+
+    if mode == 'mag':
+        y1, e1 = flux2mag(lc1.flux, lc1.err)
+        y2, e2 = flux2mag(lc2.flux, lc2.err)
+    else:
+        y1, e1 = lc1.flux, lc1.err
+        y2, e2 = lc2.flux, lc2.err
+
+    time_2d = np.tile(t1, (len(t1), 1))
+    dt_all = time_2d - np.transpose(time_2d)     
+
+    x_wmin_2d = np.tile(y1, (len(y1), 1))
+    dx_wmin_all = x_wmin_2d - np.transpose(x_wmin_2d)
+
+    x_wmax_2d = np.tile(y2, (len(y2), 1))
+    dx_wmax_all = x_wmax_2d - np.transpose(x_wmax_2d)
+
+    idx = np.where(dt_all > 0, True, False)
+
+    dt_all = dt_all[idx]
+    dx_wmin_all = dx_wmin_all[idx]
+    dx_wmax_all = dx_wmax_all[idx]
+
+    theta = dx_wmax_all / dx_wmin_all
+
+    num_all = dt_all
+
+    if erron:
+        ex_wmin_1e = np.tile(e1, (len(e1), 1))
+        ex_wmax_1e = np.tile(e2, (len(e2), 1))
+
+        ex_wmin_2e, ex_wmax_2e = np.transpose(ex_wmin_1e), np.transpose(ex_wmax_1e)
+        
+        ex_wmin_1e, ex_wmax_1e = ex_wmin_1e[idx], ex_wmax_1e[idx]
+        ex_wmin_2e, ex_wmax_2e = ex_wmin_2e[idx], ex_wmax_2e[idx]
+
+        dx_ratio2 = (dx_wmax_all / dx_wmin_all) ** 2
+        mmd = np.sqrt(
+            dx_wmin_all ** 2 + dx_wmax_all ** 2
+        )
+
+        err_mmd1 = ex_wmin_1e * ex_wmax_1e * np.sqrt((1 + dx_ratio2) / (ex_wmax_1e ** 2 + ex_wmin_1e ** 2 * dx_ratio2))
+        err_mmd2 = ex_wmin_2e * ex_wmax_2e * np.sqrt((1 + dx_ratio2) / (ex_wmax_2e ** 2 + ex_wmin_2e ** 2 * dx_ratio2)) 
+
+        err_mmd = np.sqrt(err_mmd1 ** 2 + err_mmd2 ** 2)
+
+        err_mmd_nsigma = nsigma * err_mmd
+
+        criterion = np.where(mmd >= err_mmd_nsigma, True, False)
+
+        dt_all = dt_all[criterion]
+        theta = theta[criterion]
+        num_cut = dt_all
+
+    int0 = np.argsort(dt_all, kind='mergesort')
+    dt_all_sort = dt_all[int0]
+    theta_sort = theta[int0]
+
+    idx_pos = np.where(theta_sort > 0, True, False)
+
+    dt_all_pos, theta_sort_pos = dt_all_sort[idx_pos], theta_sort[idx_pos]
+
+    num_pos = dt_all_pos
+
+    bin_dt_width = np.array([(used_bins[i+1] - used_bins[i]) / 2 for i in range(len(used_bins) - 1)])
+    bin_dt_center = used_bins[1:] - bin_dt_width
+
+    bin_cv = binned_statistic(dt_all_pos, theta_sort_pos, statistic='median', bins=used_bins)[0]
+
+    bin_cv_err = binned_statistic(dt_all_pos, theta_sort_pos, statistic=lambda x : err_std(x), bins=used_bins)[0]
+
+    if showhist:
+        return binned_data_cv(bin_dt_center, bin_dt_width, bin_cv, bin_cv_err, num_all, num_cut, num_pos)
+    else:
+        return binned_data(bin_dt_center, bin_dt_width, bin_cv, bin_cv_err)
+    
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
